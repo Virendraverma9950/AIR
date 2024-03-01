5d46f97195c4cb1272c01dfdb012854f16f6223b
ojha111 <152144787+ojha111@users.noreply.github.com>
2024-03-01 12:30:32 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+API_ID = API_ID = 21408627
+
+API_HASH = os.environ.get("API_HASH", "465cca6fa782695983d49db306e0f8be")
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6262497675:AAGut8FTPNWsEgCm6S4B_x7a7mFwKuxbIEI")
+
+PASS_DB = int(os.environ.get("PASS_DB", "721"))
+
+OWNER = int(os.environ.get("OWNER",6381511858))
+
+LOG = -1002034227984
+
